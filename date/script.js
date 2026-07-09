785f9c5d33de46ee073057d3a98ee399bc0c7ad6
remzaaaa <hamzachamrouk7@gmail.com>
2026-07-09 19:58:53 +0200

    optimisation, on veut savoir le nombre de jours uniquement du mois actuel (ajout de tests)

--- date/script.js
+++ date/script.js
@@ -9,7 +9,8 @@ const [monthNumber, dayNumber, yearNumber] = [
     date.getDate(),
     date.getFullYear(),
 ]
-const monthDaysNumber = Array(12);
+
+// const monthDaysNumber = Array(12);
 // [31], [28/29], ...
 
 function isLeapYear(year){
@@ -17,30 +18,97 @@ function isLeapYear(year){
     return year % 4 === 0 && (year % 100 !== 0 || 400 === 0);
 }
 
-function setMonthDays(array){
-    for (let i = 0; i < monthDaysNumber.length; i++) {
-        if (i === 1) { // february is special.
-            monthDaysNumber[i] = isLeapYear(2026) ? 29 : 28;
-            continue;
-        }
-        if (i > 6) { // logic reverse at July.
-            monthDaysNumber[i] = i % 2 === 0 ? 30 : 31;
-            continue;
-        }
-        monthDaysNumber[i] = i % 2 === 0 ? 31 : 30;
+// function setMonthDays(array){
+//     for (let i = 0; i < monthDaysNumber.length; i++) {
+//         if (i === 1) { // february is special.
+//             monthDaysNumber[i] = isLeapYear(2026) ? 29 : 28;
+//             continue;
+//         }
+//         if (i > 6) { // logic reverse at July.
+//             monthDaysNumber[i] = i % 2 === 0 ? 30 : 31;
+//             continue;
+//         }
+//         monthDaysNumber[i] = i % 2 === 0 ? 31 : 30;
+//     }
+// }
+
+// console.log(date);
+// console.log(monthNumber);
+// console.log(dayNumber);
+// console.log(yearNumber);
+
+// console.log(monthDaysNumber);
+// setMonthDays(monthDaysNumber);
+// console.log(monthDaysNumber);
+
+
+// const currentMonthDaysNumber = monthDaysNumber[monthNumber];
+
+function howManyDayIn(month){
+    if (month === 2) { // february is special.
+        return isLeapYear(2026) ? 29 : 28;
     }
+    if (month > 7) { // logic reverse at July.
+        return month % 2 === 0 ? 31 : 30;
+    }
+    return month % 2 !== 0 ? 31 : 30;
 }
 
-console.log(date);
-console.log(monthNumber);
-console.log(dayNumber);
-console.log(yearNumber);
+function howManyDayInTest(){
+    let monthNumber, result;
 
-console.log(monthDaysNumber);
-setMonthDays(monthDaysNumber);
-console.log(monthDaysNumber);
+    monthNumber = 1; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 2; result = howManyDayIn(monthNumber);
+    console.assert(result === 28 || result === 29,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 28 ou 29.`
+    );
+    monthNumber = 3; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 4; result = howManyDayIn(monthNumber);
+    console.assert(result === 30,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 30.`
+    );
+    monthNumber = 5; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 6; result = howManyDayIn(monthNumber);
+    console.assert(result === 30,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 30.`
+    );
+    monthNumber = 7; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 8; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 9; result = howManyDayIn(monthNumber);
+    console.assert(result === 30,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 30.`
+    );
+    monthNumber = 10; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+    monthNumber = 11; result = howManyDayIn(monthNumber);
+    console.assert(result === 30,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 30.`
+    );
+    monthNumber = 12; result = howManyDayIn(monthNumber);
+    console.assert(result === 31,
+        `Erreur : howManyDayIn(${monthNumber}) retourne ${result}, attendu 31.`
+    );
+}
 
+howManyDayInTest();
 
-const currentMonthDaysNumber = monthDaysNumber[monthNumber];
+const currentMonthDaysNumber = howManyDayIn(monthNumber);
+console.log(currentMonthDaysNumber, monthNumber);
 
-console.log(currentMonthDaysNumber);
